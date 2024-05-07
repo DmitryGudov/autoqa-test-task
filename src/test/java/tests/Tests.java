5138a8782d0a44cf6b3d1628c9f53e6af86ba71b
Dmitry Gudov <dgudoff@yandex.ru>
2024-05-07 22:54:52 +0300

    added DocumentsPage.java and modified Tests.java

---
+++ src/test/java/tests/Tests.java
@@ -0,0 +1,44 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import io.qameta.allure.Step;
+import managers.ConfigManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import pages.LoginPage;
+import pages.DocumentsPage;
+
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.Assert.assertEquals;
+
+public class Tests {
+
+    private LoginPage loginPage = new LoginPage();;
+    private DocumentsPage documentsPage = new DocumentsPage();
+
+    private String expectedUrl = ConfigManager.getProperty("documentsPageUrl");;
+
+    String email = ConfigManager.getProperty("email");
+    String password = ConfigManager.getProperty("password");
+
+    @Before
+    public void setUp() {
+        Configuration.browser = "chrome";
+        Configuration.timeout = 10_000;
+    }
+
+    @Test
+    @Step("Выполнить вход в ЛК кадровика.")
+    public void testSuccessfulLogin() {
+        loginPage.login(email, password);
+        open(expectedUrl);
+        assertEquals(expectedUrl, webdriver().driver().getWebDriver().getCurrentUrl());
+        documentsPage.searchEmployee("Орлов Д.");
+    }
+
+    @After
+    public void tearDown() {
+        closeWebDriver();
+    }
+}
