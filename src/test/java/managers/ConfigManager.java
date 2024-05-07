de698d4934fb4fb68565a016904e7f0a6becc778
Dmitry Gudov <dgudoff@yandex.ru>
2024-05-07 11:46:01 +0300

    added ConfigManager.java

---
+++ src/test/java/managers/ConfigManager.java
@@ -0,0 +1,23 @@
+package managers;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigManager {
+    private static Properties properties = new Properties();
+
+    static {
+        try {
+            FileInputStream input = new FileInputStream("src/test/java/resources/config.properties");
+            properties.load(input);
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
