66d4869fd4dd6b947064113a84380fb7b232ee03
Dmitry Gudov <dgudoff@yandex.ru>
2024-05-09 00:08:47 +0300

    modified files

--- src/test/java/managers/ApiManager.java
+++ src/test/java/managers/ApiManager.java
@@ -1,4 +1,27 @@
 package managers;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
 public class ApiManager {
+
+    private static final String BASE_URL = ConfigManager.getProperty("baseUrlForApi");
+    private static final String USER_API_TOKEN = ConfigManager.getProperty("userApiToken");
+    private static final String CLIENT_ID = ConfigManager.getProperty("clientId");
+
+    static {
+        RestAssured.baseURI = BASE_URL;
+    }
+
+    public Response searchEmployees(String search) {
+        return given()
+                .header("User-Api-Token", USER_API_TOKEN)
+                .pathParam("id", CLIENT_ID)
+                .queryParam("type", "HR")
+                .queryParam("search", search)
+                .when()
+                .get("/clients/{id}/documentRegistryFilters/employees");
+    }
 }
--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -2,17 +2,23 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import io.qameta.allure.Step;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import managers.ApiManager;
 import managers.ConfigManager;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.LoginPage;
 import pages.DocumentsPage;
 
+import java.util.List;
+import java.util.Map;
+
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.driver;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class Tests {
@@ -20,16 +26,26 @@ public class Tests {
     private LoginPage loginPage = new LoginPage();
     private DocumentsPage documentsPage = new DocumentsPage();
 
-    private String loginPageUrl = ConfigManager.getProperty("loginPageUrl");
-    private String documentsPageUrl = ConfigManager.getProperty("documentsPageUrl");
+    private String loginPageUrl;
+    private String documentsPageUrl;
+    private String email;
+    private String password;
 
-    private String email = ConfigManager.getProperty("email");
-    private String password = ConfigManager.getProperty("password");
+    ApiManager apiManager = new ApiManager();
 
     @Before
     public void setUp() {
         Configuration.browser = "chrome";
         Configuration.timeout = 10_000;
+
+        loadConfigProperties();
+    }
+
+    private void loadConfigProperties() {
+        loginPageUrl = ConfigManager.getProperty("loginPageUrl");
+        documentsPageUrl = ConfigManager.getProperty("documentsPageUrl");
+        email = ConfigManager.getProperty("email");
+        password = ConfigManager.getProperty("password");
     }
 
     @Test
@@ -48,16 +64,26 @@ public class Tests {
 
     @Step("Поиск по фильтру 'Сотрудник' в реестре документов")
     public void testSearchEmployee() {
-        documentsPage.searchEmployee("Орлов Д.а");
-        if (documentsPage.getNotFoundEmployee().isDisplayed()) {
-            String actualText = documentsPage.notFoundEmployeeString();
-            assertTrue(actualText.contains("Ненайдено"));
+        String search = "Орлов Д.";
+        Response response = apiManager.searchEmployees(search);
+        Assertions.assertEquals(200, response.getStatusCode());
+        JsonPath jsonPath = response.jsonPath();
+        boolean result = jsonPath.getBoolean("result");
+        Assertions.assertTrue(result);
+
+        List<Map<String, Object>> employees = jsonPath.getList("employees");
+        if (employees.isEmpty()) {
+            Assertions.assertTrue(employees.isEmpty());
         } else {
-
+            Map<String, Object> firstEmployee = employees.get(0);
+            Map<String, Object> legalEntity = (Map<String, Object>) firstEmployee.get("legalEntity");
+            Assertions.assertNotNull(legalEntity);
+            String legalEntityName = (String) legalEntity.get("name");
+            Assertions.assertNotEquals("ООО \"Кот\"", legalEntityName);
+            String legalEntityShortName = (String) legalEntity.get("shortName");
+            Assertions.assertNotEquals("ООО \"Кот\"", legalEntityShortName);
         }
-
     }
-
 
     @After
     public void tearDown() {
