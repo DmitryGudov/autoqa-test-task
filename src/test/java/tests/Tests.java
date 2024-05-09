19a6b2fafa3fd71bdcdf82fe909f79f19411cd53
Dmitry Gudov <dgudoff@yandex.ru>
2024-05-09 13:50:55 +0300

    added ApplicetionsPage.java and methods related to him

--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pages.ApplicationsPage;
 import pages.LoginPage;
 import pages.DocumentsPage;
 
@@ -25,9 +26,12 @@ public class Tests {
 
     private LoginPage loginPage = new LoginPage();
     private DocumentsPage documentsPage = new DocumentsPage();
+    private ApplicationsPage applicationsPage = new ApplicationsPage();
+
 
     private String loginPageUrl = ConfigManager.getProperty("loginPageUrl");
     private String documentsPageUrl = ConfigManager.getProperty("documentsPageUrl");
+    private String applicationsPageUrl = ConfigManager.getProperty("applicationsPageUrl");
     private String email = ConfigManager.getProperty("email");
     private String password = ConfigManager.getProperty("password");
 
@@ -44,8 +48,9 @@ public class Tests {
     @DisplayName("Проверка недоступности юрлица, на которое у кадровика нет прав")
     public void test() {
         testSuccessfulLogin();
-        testSearchEmployee();
+        testSearchEmployeeInDocuments();
         testSearchLegalEntity();
+        testSearchEmployeeInApplications();
     }
 
     @Step("Выполнить вход в ЛК кадровика")
@@ -56,7 +61,7 @@ public class Tests {
     }
 
     @Step("Поиск по фильтру 'Сотрудник' в реестре документов")
-    public void testSearchEmployee() {
+    public void testSearchEmployeeInDocuments() {
         // Проверка наличия сотрудника в фильтре "Сотрудник"
         String search = "Орлов Д.";
         Response response = apiManager.searchEmployees(search);
@@ -93,6 +98,24 @@ public class Tests {
         documentsPage.notFoundLegalEntity();
         String actualText = documentsPage.notFoundLegalEntityString();
         assertTrue(actualText.contains("Ненайдено"));
+    }
+
+    @Step("Открыть реестр заявлений")
+    public void testSearchEmployeeInApplications() {
+        applicationsPage.searchEmployee("Орлов Д.ю");
+        applicationsPage.notFoundEmployee();
+        String actualText = applicationsPage.notFoundEmployeeString();
+        assertTrue(actualText.contains("Ненайдено"));
+
+        // Проверка наличия заявления в реестре по искомому сотруднику
+        String employeeId = "962f94c2-ac7c-45c2-864a-9e3bc484be5d";
+        Response response = apiManager.searchEmployeeInHRRegistryApplications(employeeId);
+        Assertions.assertEquals(200, response.getStatusCode(), "Статус ответа должен быть 200");
+        JsonPath jsonPath = response.jsonPath();
+        Assertions.assertTrue(jsonPath.getBoolean("result"), "Поле 'result' должно быть true");
+        List<Object> applicationGroups = jsonPath.getList("applicationGroups");
+        Assertions.assertNotNull(applicationGroups, "Поле 'applicationGroups' должно быть в ответе");
+        Assertions.assertTrue(applicationGroups.isEmpty(), "Поле 'applicationGroups' должно быть пустым");
     }
 
     @After
