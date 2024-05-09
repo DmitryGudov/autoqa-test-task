7720ba38f9ac6ba3129b7d7bdb9003c7a105aa4b
Dmitry Gudov <dgudoff@yandex.ru>
2024-05-09 12:30:07 +0300

    added methods:  searchEmployeeInHRRegistry, searchLegalEntity, notFoundLegalEntity, notFoundLegalEntityString; supplemented Tests.java

--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -26,10 +26,10 @@ public class Tests {
     private LoginPage loginPage = new LoginPage();
     private DocumentsPage documentsPage = new DocumentsPage();
 
-    private String loginPageUrl;
-    private String documentsPageUrl;
-    private String email;
-    private String password;
+    private String loginPageUrl = ConfigManager.getProperty("loginPageUrl");
+    private String documentsPageUrl = ConfigManager.getProperty("documentsPageUrl");
+    private String email = ConfigManager.getProperty("email");
+    private String password = ConfigManager.getProperty("password");
 
     ApiManager apiManager = new ApiManager();
 
@@ -37,22 +37,15 @@ public class Tests {
     public void setUp() {
         Configuration.browser = "chrome";
         Configuration.timeout = 10_000;
-
-        loadConfigProperties();
     }
 
-    private void loadConfigProperties() {
-        loginPageUrl = ConfigManager.getProperty("loginPageUrl");
-        documentsPageUrl = ConfigManager.getProperty("documentsPageUrl");
-        email = ConfigManager.getProperty("email");
-        password = ConfigManager.getProperty("password");
-    }
 
     @Test
     @DisplayName("Проверка недоступности юрлица, на которое у кадровика нет прав")
     public void test() {
         testSuccessfulLogin();
         testSearchEmployee();
+        testSearchLegalEntity();
     }
 
     @Step("Выполнить вход в ЛК кадровика")
@@ -64,6 +57,7 @@ public class Tests {
 
     @Step("Поиск по фильтру 'Сотрудник' в реестре документов")
     public void testSearchEmployee() {
+        // Проверка наличия сотрудника в фильтре "Сотрудник"
         String search = "Орлов Д.";
         Response response = apiManager.searchEmployees(search);
         Assertions.assertEquals(200, response.getStatusCode());
@@ -79,10 +73,26 @@ public class Tests {
             Map<String, Object> legalEntity = (Map<String, Object>) firstEmployee.get("legalEntity");
             Assertions.assertNotNull(legalEntity);
             String legalEntityName = (String) legalEntity.get("name");
-            Assertions.assertNotEquals("ООО \"Кот\"", legalEntityName);
+            Assertions.assertNotEquals("ООО \"Коот\"", legalEntityName);
             String legalEntityShortName = (String) legalEntity.get("shortName");
-            Assertions.assertNotEquals("ООО \"Кот\"", legalEntityShortName);
+            Assertions.assertNotEquals("ООО \"Коот\"", legalEntityShortName);
         }
+
+        // Проверка наличия документа в реестре по искомому сотруднику
+        String employeeId = "962f94c2-ac7c-45c2-864a-9e3bc484be5d";
+        Response hrRegistryResponse = apiManager.searchEmployeeInHRRegistry(employeeId);
+        Assertions.assertEquals(200, hrRegistryResponse.getStatusCode());
+        JsonPath hrJsonPath = hrRegistryResponse.jsonPath();
+        List<Object> documents = hrJsonPath.getList("documents");
+        Assertions.assertTrue(documents.isEmpty());
+    }
+
+    @Step("В верхнем фильтре нажать на фильтр \"Юрлицо\"")
+    public void testSearchLegalEntity() {
+        documentsPage.searchLegalEntity("ООО \"Коот\"");
+        documentsPage.notFoundLegalEntity();
+        String actualText = documentsPage.notFoundLegalEntityString();
+        assertTrue(actualText.contains("Ненайдено"));
     }
 
     @After
